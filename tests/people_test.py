68fb98fb635e37a32592eb3185d8ad428fd9f1b3
Jorge Ardila <Jardilac91@gmail.com>
2021-11-22 23:07:59 -0500

    Clase 5 - Trabajando con json

--- tests/people_test.py
+++ tests/people_test.py
@@ -1,23 +1,29 @@
-from json import dumps
+import random
+from json import dumps, loads
 from uuid import uuid4
+
+import pytest
+import requests
 from assertpy.assertpy import assert_that
+from jsonpath_ng import parse
+
 from utils.config import BASE_URI
 from utils.print_helpers import pretty_print
-import requests
+from utils.file_reader import read_file
 
 
 def test_read_all_has_kent():
     peoples, response = get_all_users()
-    #Se verifica que la url responda con un status code 200
+    # Se verifica que la url responda con un status code 200
     assert_that(response.status_code).is_equal_to(requests.codes.ok)
-    #Se almacenan los primeros nombres en una lista
+    # Se almacenan los primeros nombres en una lista
     first_names = [people['fname'] for people in peoples]
-    #Se comprueba que en la lista existe un usuario con nombre Kent
+    # Se comprueba que en la lista existe un usuario con nombre Kent
     assert_that(first_names).contains('Kent')
 
 
 def test_add_new_user():
-    unique_last_name = create_new_user()
+    unique_last_name = create_new_user_with_unique_last_name()
 
     peoples = requests.get(BASE_URI).json()
     new_user_created = search_users_by_last_name(peoples, unique_last_name)
@@ -29,7 +35,7 @@ def search_users_by_last_name(peoples, unique_last_name):
 
 
 def test_delete_person():
-    new_user_lastname = create_new_user()
+    new_user_lastname = create_new_user_with_unique_last_name()
     all_users, _ = get_all_users()
     new_user = search_users_by_last_name(all_users, new_user_lastname)[0]
 
@@ -50,12 +56,41 @@ def get_all_users():
     return peoples, response
 
 
-def create_new_user():
-    unique_last_name = f'User {str(uuid4())}'
-    payload = dumps({
-        'fname': 'New',
-        'lname': unique_last_name
-    })
+@pytest.fixture
+def create_data():
+    payload = read_file('create_person.json')
+
+    random_no = random.randint(0,1000)
+    last_name = f'Ardila{random_no}'
+
+    payload['lname'] = last_name
+    yield payload
+
+
+def test_person_can_be_added_with_a_json_template(create_data):
+    create_new_user_with_unique_last_name(create_data)
+
+    response = requests.get(BASE_URI)
+    peoples = loads(response.text)
+
+    jsonpath_expr = parse("$.[*].lname")
+    result = [match.value for match in jsonpath_expr.find(peoples)]
+
+    expected_last_name = create_data['lname']
+    assert_that(result).contains(expected_last_name)
+
+
+def create_new_user_with_unique_last_name(body=None):
+    if body is None:
+        unique_last_name = f'User {str(uuid4())}'
+        payload = dumps({
+            'fname': 'New',
+            'lname': unique_last_name
+        })
+    else:
+        unique_last_name = body['lname']
+        payload = dumps(body)
+
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
